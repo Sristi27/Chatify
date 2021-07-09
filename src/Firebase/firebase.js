20b03cfb1c798f616287ddcc105f74a038ee8030
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-07-09 22:00:19 +0530

    updated firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -4,10 +4,10 @@ import "firebase/auth";
 
 //function call
 export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyC6ShN2I6AbrubmBSTWC8JKK78IcxDutXQ",
-    authDomain: "chatapp-450f1.firebaseapp.com",
-    projectId: "chatapp-450f1",
-    storageBucket: "chatapp-450f1.appspot.com",
-    messagingSenderId: "620795967351",
-    appId: "1:620795967351:web:2a1d6971170991accf5372"
+    apiKey: your_api_key,
+    authDomain: your_auth_domain,
+    projectId: your_proj_id ,
+    storageBucket: your_storage_bucket,
+    messagingSenderId: your_sender_id,
+    appId: your_app_id
   }).auth();
